4b5e5f80ff17bf702471ed0db5ea9799cc707e32
Murad Murtuzaliev <mur.murtuzaliev@yandex.ru>
2024-02-19 00:07:50 +0300

    feat: complete raw matrix input/output

--- math_package/io/matrix.py
+++ math_package/io/matrix.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
 from enum import Enum
+from typing import Any
 
 from math_package.configuration import ERR, OUT
+from math_package.io.exception import WrongRowsNumberException, WrongStringTypeException
 from math_package.matrix import Matrix
 
 
@@ -25,20 +27,21 @@ class MatrixReader:
     def process_raw_matrix(self, raw_matrix: list[str]) -> Matrix:
         pass
 
-    def read(self, input_type: InputType) -> Matrix:
+    def read(self) -> Matrix:
         raw_matrix = self.read_raw_matrix()
         return self.process_raw_matrix(raw_matrix)
 
 
 class RawMatrixReader(ABC):
     @abstractmethod
-    def read(self) -> list[str]:
+    def read(self) -> list[Any]:
         pass
 
 
 class MatrixReaderFromFile(RawMatrixReader):
-    def read(self) -> list[str]:
-        """ Возвращает считанную с клавиатуры матрицу в виде массива строк.
+    def read(self) -> list[Any]:
+        """ Возвращает считанную с из файла матрицу в виде массива строк.
+        Первая строка файла должна содержать количество строк, вторая - столбцов.
 
         :return: список состоящий из строк матрицы
         """
@@ -46,24 +49,69 @@ class MatrixReaderFromFile(RawMatrixReader):
         filename = input()
 
         try:
+            raw_matrix = []
+            lines = []
             with open(filename, "r") as file:
                 lines = file.readlines()
-                if lines is not bytes:
-                    return lines
-                print("Ошибка! Из файла считаны байтовые строки", file=ERR)
-                return []
+                if lines is bytes:
+                    raise WrongStringTypeException
+
+            lines_count, rows_count = int(lines[0]), int(lines[1])
+            lines = lines[2:]
+            for i in range(lines_count):
+                current_line = lines[i].strip().split()
+                if len(current_line) != rows_count:
+                    raise WrongRowsNumberException
+                raw_matrix.append(current_line)
+            return raw_matrix
         except FileNotFoundError:
-            print("Ошибка! Файл не найдет", file=ERR)
+            print("Ошибка! Файл не найден", file=ERR)
+            return []
+        except WrongStringTypeException:
+            print("Ошибка! В строке введено некорректное число элементов", file=ERR)
+            return []
+        except (ValueError, WrongRowsNumberException):
+            print("Ошибка! Введите корректные количество строк и столбцов", file=ERR)
             return []
 
 
 class MatrixReaderFromKeyboard(RawMatrixReader):
-    def read(self) -> list[str]:
-        hint = "Введите количество %s:"
-        while True:
-            try:
-                print(hint.format("строк"), file=OUT)
-                rows = int(input())
+    def read(self) -> list[Any]:
+        """ Возвращает считанную с клавиатуры матрицу в виде массива строк.
 
-            except ValueError:
-                print("")
+        :return: список состоящий из строк матрицы
+        """
+        hint = "Введите количество {}:"
+        try:
+            print(hint.format("строк"), file=OUT)
+            rows_count = input()
+
+            print(hint.format("столбцов"), file=OUT)
+            lines_count = input()
+
+            if not (rows_count.isdecimal() or lines_count.isdecimal()):
+                raise ValueError
+            rows_count, lines_count = int(rows_count), int(lines_count)
+
+            print("Введите матрицу:", file=OUT)
+            raw_matrix = []
+            for i in range(lines_count):
+                current_line = input().strip().split()
+                if len(current_line) != rows_count:
+                    raise WrongRowsNumberException
+                raw_matrix.append(current_line)
+            return raw_matrix
+
+        except ValueError:
+            print("Ошибка! Введите целые числа в десятичной системе счисления", file=ERR)
+            return []
+        except WrongRowsNumberException:
+            print("Ошибка! В строке введено некорректное число элементов", file=ERR)
+            return []
+
+
+class RawMatrixPrinter:
+    @staticmethod
+    def print(raw_matrix: list[Any]) -> None:
+        for line in raw_matrix:
+            print(line, file=OUT)
